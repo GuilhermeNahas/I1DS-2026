c8ccb9705fb0eff716784133fab7a46ae2a6de0a
Guilherme Gimenez Nahas <guilhermegimeneznahas.senai@gmail.com>
2026-03-11 16:45:44 -0300

    Operadores lógicos

--- aulas/05-operadoresLogicos.js
+++ aulas/05-operadoresLogicos.js
@@ -7,6 +7,16 @@ console.log("Negação de souPobre:", !souPobre);
 let manha = true;
 let sono = true;
 console.log("-----------------------------------------");
-console.log("Manhã:", manha,  "| Estou com sono?", sono)
+console.log("Manhã:", manha, "| Estou com sono?", sono);
 
-console.log("Operador AND (E)", )
+//Operador E AND e OR
+console.log("Operador AND (E):", manha && sono);
+console.log("Operador OR (OU):", manha || sono);
+
+manha = false
+console.log("-----------------------------------------");
+console.log("Manhã:", manha, "| Estou com sono?", sono);
+
+//Operador E AND e OR
+console.log("Operador AND (E):", manha && sono);
+console.log("Operador OR (OU):", manha || sono);
