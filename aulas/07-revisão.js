1f39cf15f841e045e330375c701cdd9b9116217c
Guilherme Gimenez Nahas <guilhermegimeneznahas.senai@gmail.com>
2026-03-17 09:26:57 -0300

    Resuma sobre os conteudos vistos em aula

---
+++ aulas/07-revisão.js
@@ -0,0 +1,50 @@
+//Comentario em linha
+
+/*
+Comentario de varias linhas
+bloco de código
+*/
+
+//Imprimir informações na tela
+console.log("Imprime informações na tela");
+
+/*declaração de variáveis
+Sintaxe: let tipo-variável = valor-variavel
+exemplo
+let idade = 45;
+*/
+
+/* tipo de dados primários
+   numeros inteiros (int) = 10
+   numeros decimais (double) = 8.5
+   caracteres alfa numéricos (string) = "Cristiano"
+   numeros booleanos(bool) = false
+*/
+let idade = 45;
+let salario = 25864.57;
+let ehProfessor = false;
+let nome = "Joao da Silva";
+
+console.log("Nome:", nome, "Idade:", idade, "salário:", salario);
+
+/*
+Operadores matemáticos 
++ Adição
+- subtração
+/ divisão 
+* multiplicação
+% resto da divisão
+*/
+
+let num1 = 10;
+let num2 = 2;
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let divisao = num1 / num2;
+let multiplicacao = num1 * num2;
+let restoDivisao = num1 % num2;
+
+//Imprimir as operações
+console.log("Somando ", num1, " + ", num2, " = ", soma);
+console.log("O resto da divisão do", num1, " por ", num2, " = ", restoDivisao);
+
