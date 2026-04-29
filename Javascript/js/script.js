1f0a608d0e674b891f5d710fb9fa99ece212a6d6
GuilhermeNahas <guilhermegimeneznahas.senai@gmail.com>
2026-04-29 11:27:38 -0300

    Calculadora basica feita com JS, introdução de novos conteudos

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -1,26 +1,54 @@
-//variavel para armazenar o elemento <p> com id = contador
+// Variavel para armazenar o Elemento <p> com id=contador
 let contador = document.getElementById("contador");
 
 const adicionar = () => {
-  //variavel para armazenar o valor atual
+  // Variavel para aramazenar o valor atual
   let valorAtual = parseInt(contador.innerHTML);
-  //auemnta o valor atual em +1
+  // Aumenta o valor Atual em +1
   valorAtual++;
-  //atualizar o valor do elemento <p> com id=contador
+  // Atualizar o Valor do elmento <p> com id=contador
   contador.innerHTML = valorAtual;
 };
-
 const diminuir = () => {
-  valorAtual = parseInt(contador.innerHTML);
+  let valorAtual = parseInt(contador.innerHTML);
   valorAtual--;
   contador.innerHTML = valorAtual;
 };
-
 const zerar = () => {
-  valorAtual = parseInt(contador.innerHTML);
-  valorAtual = 0;
-  contador.innerHTML = valorAtual;
+  contador.innerHTML = 0;
 };
 
+/* EXERCICIO 02 */
+const mudarCor = () => {
+  // Capturar a cor selecionada pelo usuário
+  let cor = document.getElementById("colorPicker").value;
+  // Buscar o elemento-alvo (quadrado)
+  let quadrado = document.getElementById("quadrado");
+  // Aplicar a cor ao elemento. (Alterar o css do background-color)
+  quadrado.style.backgroundColor = cor;
+};
 
+/* EXERCICIO 03 */
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let resultado = document.getElementById("resultado");
 
+const somar = () => {
+  let valor = parseFloat(num1.value) + parseFloat(num2.value);
+  resultado.innerText = valor;
+};
+
+const subtrair = () => {
+  let valor = parseFloat(num1.value) - parseFloat(num2.value);
+  resultado.innerText = valor;
+};
+
+const multiplicar = () => {
+  let valor = parseFloat(num1.value) * parseFloat(num2.value);
+  resultado.innerText = valor;
+};
+
+const dividir = () => {
+  let valor = parseFloat(num1.value) / parseFloat(num2.value);
+  resultado.innerText = valor;
+};
